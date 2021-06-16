c8feea6a7dee26db4d0418ff61ed00dbac02cd58
RaphaelKhaykin <raphaelkhaykin@gmail.com>
2021-06-16 11:23:00 -0400

    file restructure

--- displayChart.js
+++ displayChart.js
@@ -1,66 +1,119 @@
-//Requires Chart.js and Moment.js
+//Requires D3
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: chartDateLabels,
-    datasets: [{
-      label: '# of Votes',
-      data: chartPriceData,
-      borderWidth: 1
-    }]
-  },
-  options: {}
+var chartRequested = false;
+var acceptedStocks = ['AAPL', 'BABA', 'SPY', 'TSLA']
+var stockDict ={};
+
+$(window).on("resize", function () {
+  if (chartRequested) {
+    removeChart();
+    paintChart();
+  };
 });
 
-var selected_stocks = []
-var chartPriceData = []
-var chartDateLabels = [];
+function paintChart() {
+  containerWidth = document.getElementById('chartContainer').getBoundingClientRect().width
+  // set the dimensions and margins of the graph
+  var margin = { top: 10, right: 30, bottom: 30, left: 30 },
+    width = (containerWidth * 0.9) - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
-function formatDate(date){
-    var timestamp = Date.parse(date);
-    var dateObject = new Date(timestamp);
-    var formattedDateObject = moment(dateObject).format('YYYY-MM-DD');
-    console.log(formattedDateObject);
-    return(formattedDateObject);
-};
+  // append the svg object to the body of the page
+  var svg = d3.select("#my_dataviz")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+      "translate(" + margin.left + "," + margin.top + ")");
 
-function updateSelectedStocks() {
-    var stock;
-    selected_stocks = []
-    $(".stock_input").each(function () {
-        stock = $(this).val()
-        if (selected_stocks.indexOf(stock) === -1) {
-            selected_stocks.push(stock);
-        };
-    });
-    parseCSV();
-};
+  //Read the data
+  csvPath2 = "data/tsla.csv"
+  d3.csv(csvPath2,
 
-function parseCSV() {
-    $.get("tsla.csv", function (response) {
-        var result = $.csv.toArrays(response);
-        result.forEach(function (element, index) {
-            var date = formatDate(element[0]);
-            var price = parseFloat(element[1]);
-            addChartData(myChart,date,price)
-        });
-    });
+    // When reading the csv, I must format variables:
+    function (d) {
+      return { date: d3.timeParse("%m/%d/%Y")(d.date), value: d.value }
+    },
+
+    // Now I can use this dataset:
+    function (data) {
+
+      // Add X axis --> it is a date format
+      var x = d3.scaleTime()
+        .domain(d3.extent(data, function (d) { return d.date; }))
+        .range([0, width]);
+      svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+      // Add Y axis
+      var y = d3.scaleLinear()
+        .domain([0, d3.max(data, function (d) { return +d.value; })])
+        .range([height, 0]);
+      svg.append("g")
+        .call(d3.axisLeft(y));
+
+      // Add the line
+      svg.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.line()
+          .x(function (d) { return x(d.date) })
+          .y(function (d) { return y(d.value) }))
+    })
 }
 
-function addChartData(chart, label, data) {
-    chart.data.labels.push(label);
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.push(data);
-    });
-    chart.update();
+function removeChart() {
+  d3.select("svg").remove();
 }
 
-function removeChartData(chart) {
-    chart.data.labels.pop();
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.pop();
-    });
-    chart.update();
+function chartButtonClick() {
+    chartRequested = true;
+    collectAndValidateInputData();
+    removeChart();
+    paintChart();
+};
+
+function collectAndValidateInputData(){
+  var percentSum = 0;
+  stockDict = {};
+
+  $('div[id^="stock-row"]').each(function(index) {
+    divRow = $(this)[0];
+    symbolInput = $(divRow).children().eq(0).children().eq(0)[0].value;
+    percentInput = parseInt($(divRow).children().eq(1).children().eq(0)[0].value);
+
+    //pushes symbolInput and percentInput to Dictionary
+    if (!isNaN(percentInput) && acceptedStocks.includes(symbolInput) && percentInput>=0){
+
+      //Sum percentages for duplicate symbols
+      if(symbolInput in stockDict){
+        percentSum = stockDict[symbolInput] + percentInput;
+        console.log("duplicate found: " + symbolInput + "\nNew Sum: " + percentSum);
+        stockDict[symbolInput] = percentSum;
+      }
+      else{
+         stockDict[symbolInput] = percentInput;
+      }
+    }
+    console.log(stockDict);
+    if (!isNaN(percentInput)){
+      percentSum += percentInput;
+    }
+
+    if(percentSum == 100){
+
+      //check that percentSum associated with valid symbolInput
+      var dictPercentSum = 0;
+      for (const [key, value] of Object.entries(stockDict)) {
+        dictPercentSum+=value;
+      };
+      if(dictPercentSum == 100){
+        console.log("percentSum is 100");
+      };
+    };
+  });
 }
